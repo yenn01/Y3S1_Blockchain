37c11764c5b64f0ebab3b37940dfd41a7d742427
yenn01 <yenn.m17@gmail.com>
2022-10-01 12:51:46 +0800

    Added dex component to communicate with contract

--- src/components/Dashboard.svelte
+++ src/components/Dashboard.svelte
@@ -1,10 +1,38 @@
 <script>
+    import { forEach } from '@metamask/jazzicon/colors';
+import DeX from './DeX.svelte';
+import { ethers } from "ethers";
+    import { svg_element } from 'svelte/internal';
 
+let dex;
 
+export let show;
+
+ const loadActivePools = (eventMsg) => {
+
+        console.log(eventMsg.detail)
+
+//     console.log(allActive)
+
+//     // const keys = eventMsg.detail.slice(4,7)
+//     // let parsed = JSON.parse(JSON.stringify())
+//     // console.log(parsed)
+//     // 4-7
+//     show = true;
+ }
+
+    function  addPoolTest() {
+        //dex.addPool('Aurora',15000,20000)
+        dex.exchangeBuy('Bitcoin','10','PAX Gold')
+    }
 
 </script>
+<DeX bind:this={dex} dashboard={true} on:s_getAllActivePools={loadActivePools} on:s_addPool={loadActivePools}></DeX>
+<button on:click={addPoolTest}>Add</button>
+{#if show === true}
 
 
+{/if}
 
 <style>
     
--- src/components/DeX.svelte
+++ src/components/DeX.svelte
@@ -4,28 +4,38 @@
 
     //Used to create dispatch events catched by parent components
     import { createEventDispatcher } from 'svelte';
+
     const dispatch = createEventDispatcher();
 
+    const contractAddr = '0x4460c452A40a3F33bbfb24dF65134bBBc95B38f7'
+
     const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const contract = new ethers.Contract('0x4460c452A40a3F33bbfb24dF65134bBBc95B38f7', abi.abi, provider);
+    let contract = new ethers.Contract(contractAddr, abi.abi, provider);
+   
+
     let allPools
 
-    function setSigner() {
+    function getSigner() {
+        const signer = provider.getSigner()
+        contract = new ethers.Contract(contractAddr, abi.abi, signer );
+    }
 
+    function getProvider() {
+        contract = new ethers.Contract(contractAddr, abi.abi, provider);
     }
 
     //Read Functions
     export function getAllPools() {
         contract.listAllPool().then((res)=> {
             console.log(res)
-            dispatch('s_getAllPools',res)
+            dispatch('s_getAllPools',formatTokenListResponse(res))
         })
     }
 
     export function getAllActivePools() {
         contract.listAllValidPool().then((res)=> {
             console.log(res)
-            dispatch('s_getAllActivePools',res)
+            dispatch('s_getAllActivePools',formatTokenListResponse(res))
         })
     }
 
@@ -35,6 +45,72 @@
             dispatch('s_getPool',res)
         })
     }
+
+
+    //Write Functions
+    export function addPool(name,val,against) {
+        getSigner()
+        contract.addPool(name,val,against).then((res)=> {
+            dispatch('s_addPool',res)
+        })
+    }
+
+    export function addAmt(name,val,against) {
+        getSigner()
+        contract.addAmt(name,val,against).then((res)=> {
+            dispatch('s_addAmt',res)
+        })
+    }
+
+    export function editPoolDetail(name,newname,val,against,status) {
+        getSigner()
+        contract.addAmt(name,val,against).then((res)=> {
+            dispatch('s_addAmt',res)
+        })
+    }
+
+    export function exchangeBuy(deposit,amt,target) {
+        getSigner()
+        contract.exchangeBuy(deposit,amt,target).then((res)=> {
+            dispatch('s_exchangeBuy',res)
+        })
+    }
     
+    export function exchangeSell(deposit,amt,target) {
+        getSigner()
+        contract.exchangeSell(deposit,amt,target).then((res) => {
+            dispatch('s_exchangeSell',res)
+        })
+    }
+
+    export let dashboard;
+
+    if( dashboard == true) {
+        getAllActivePools()
+
+    }
+
+
+
+    function formatTokenListResponse(response) {
+        let allActive = [];
+        response.forEach((item,i) => {
+
+            const obj = {}
+            obj['name'] = item[0]
+            obj['coinAmount'] =  item[1].toNumber()
+            obj['tokenAmount'] = item[2].toNumber()
+            obj['status'] = item[3]
+        
+            console.log(obj)
+
+            //console.log(item)
+            // const cut = item.slice(4,7)
+            //const parsed = JSON.parse(JSON.stringify(item))
+            allActive.push(obj)
+            //console.log(parsed)
+        })
+        return allActive
+    }
 
 </script>
