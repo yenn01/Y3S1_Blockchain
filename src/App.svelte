849e3047f7ba181ad54fd4a7dcdf40b6c57ccea3
yenn01 <yenn.m17@gmail.com>
2022-10-03 22:22:07 +0800

    Added title and favicon

--- src/App.svelte
+++ src/App.svelte
@@ -11,6 +11,8 @@
 </script>
 
 <svelte:head>
+    <title>/swap</title>
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
  	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
